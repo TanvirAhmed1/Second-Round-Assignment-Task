c301c0987f2d943a1ecde2e5a5e3f75d2bf1acab
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2023-01-09 23:50:15 +0600

    Employee Data Model Created

---
+++ Second-Round-Assignment-Task/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Second_Round_Assignment_Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
---
+++ Second-Round-Assignment-Task/DataModels/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Second_Round_Assignment_Task.DataModels
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Designation { get; set; }
+        public string Department { get; set; }
+    }
+}
