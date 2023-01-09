86d8049d3a28ef85c7d1c14fb94568aa0ad990cd
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2023-01-10 01:31:21 +0600

    Employee Repository In Progress

---
+++ Second-Round-Assignment-Task/Repositories/EmployeesRepository.cs
@@ -0,0 +1,39 @@
+﻿using Second_Round_Assignment_Task.DataModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Second_Round_Assignment_Task.Repositories
+{
+    public class EmployeesRepository : IEmployeeRepository
+    {
+        public Task<Employee> AddEmployee(Employee employee)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Employee> DeleteEmployee(int employeeId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> Exists(int employeeId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeAsync(int employeeId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<Employee>> GetEmployeesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Employee> UpdateEmployee(int employeeId, Employee employee)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Second-Round-Assignment-Task/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using System;
+using Second_Round_Assignment_Task.DataModels;
+
+namespace Second_Round_Assignment_Task.Repositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<Employee>> GetEmployeesAsync();
+        Task<Employee> GetEmployeeAsync(int employeeId);
+        Task<bool> Exists(int employeeId);
+        Task<Employee> UpdateEmployee(int employeeId, Employee employee);
+        Task<Employee> DeleteEmployee(int employeeId);
+        Task<Employee> AddEmployee(Employee employee);
+    }
+}
